0ec7f14dfe0166ad896310b75674552e468e7eb1
steve <2226069315@qq.com>
2024-07-06 08:59:44 +0800

    admin redirection

--- js/file.CArma9xP.js
+++
@@ -1,1 +0,0 @@
-import{b9 as t}from"./index.DFlZ7dGX.js";function e(e){return t({url:"/system/file/upload",method:"POST",headers:{"Content-Type":"multipart/articles-data"},data:e})}export{e as u};
--- js/strings.Crw7MV9D.js
+++
@@ -1,1 +0,0 @@
-import{dh as e}from"./index.DFlZ7dGX.js";const a=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),r=a=>e(a);export{r as c,a as e};
